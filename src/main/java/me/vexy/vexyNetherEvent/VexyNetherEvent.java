305306d5ce562f72c0624c4beac01dee028de73f
Daniel <90870585+VexyPT@users.noreply.github.com>
2024-10-18 23:18:01 +0100

    first commit

---
+++ src/main/java/me/vexy/vexyNetherEvent/VexyNetherEvent.java
@@ -0,0 +1,17 @@
+package me.vexy.vexyNetherEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VexyNetherEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
